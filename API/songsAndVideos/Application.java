76276bae37f301566a6f2535d83b1f248d78411b
dyoon17 <nkdy08@gmail.com>
2025-02-06 02:52:59 +0900

    Update Application.java

--- API/songsAndVideos/Application.java
+++ API/songsAndVideos/Application.java
@@ -2,8 +2,10 @@ package com.example._4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example._4") // 패키지 전체를 검색
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
