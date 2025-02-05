b6b678e6ff68a15290578ef0d58965d506813dc8
kymEwhain <ymdwang200@ewhain.net>
2025-02-05 16:36:26 +0900

    Update AuthController.java

--- API/authAnduser/controller/AuthController.java
+++ API/authAnduser/controller/AuthController.java
@@ -9,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,11 +26,13 @@ public class AuthController {
     private final JWTUtil jwtUtil;
 
 
-    /** 회원 가입 **/
+    /**
+     * 회원 가입
+     **/
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody @Valid JoinRequest joinRequest, BindingResult bindingResult){
+    public ResponseEntity<?> join(@RequestBody @Valid JoinRequest joinRequest, BindingResult bindingResult) {
         // 회원가입 폼 오류 (전체 필드 유효성 검사)
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             List<String> errorMessages = bindingResult.getAllErrors().stream()
                     .map(error -> error.getDefaultMessage())
                     .collect(Collectors.toList());
@@ -36,18 +40,18 @@ public class AuthController {
         }
 
         // 비밀번호 확인 불일치
-        if(!joinRequest.getPassword1().equals(joinRequest.getPassword2())){
+        if (!joinRequest.getPassword1().equals(joinRequest.getPassword2())) {
             return ResponseEntity.badRequest().body("The passwords do not match.");
         }
 
         // 사용자 생성
-        try{
+        try {
             this.userService.create(joinRequest.getName(), joinRequest.getEmail(), joinRequest.getPassword1());
-        }catch(DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             // 중복 사용자 오류 처리
             e.printStackTrace();
             return ResponseEntity.badRequest().body("User already exists.");
-        }catch (Exception e){
+        } catch (Exception e) {
             // 다른 예외 처리
             e.printStackTrace();
             return ResponseEntity.badRequest().body("An unexpected error occurred: " + e.getMessage());
@@ -57,15 +61,15 @@ public class AuthController {
     }
 
 
-
-    /** 로그인 토큰 
+    /**
+     * 로그인 토큰
      * 헤더의 토큰을 확인하기 위한 테스트 메소드
      * 나중에 제거
-     * **/
+     **/
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest, BindingResult bindingResult){
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest, BindingResult bindingResult) {
         // 로그인 폼 오류 (전체 필드 유효성 검사)
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             List<String> errorMessages = bindingResult.getAllErrors().stream()
                     .map(error -> error.getDefaultMessage())
                     .collect(Collectors.toList());
@@ -75,8 +79,11 @@ public class AuthController {
         return ResponseEntity.ok().body("The token in the header was verified.");
     }
 
-
-
+    @GetMapping({"/loginNaver", "/loginGoogle"})
+    public ModelAndView loginOauth(){
+        ModelAndView modelAndView = new ModelAndView("socialLogin"); // "socialLogin" 뷰를 반환
+        return modelAndView;
+    }
 
 
 
