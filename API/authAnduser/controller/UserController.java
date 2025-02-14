1ea3a954ac441c46332600f1f447357161ed12b7
kymEwhain <ymdwang200@ewhain.net>
2025-02-14 13:18:05 +0900

    Update UserController.java

--- API/authAnduser/controller/UserController.java
+++ API/authAnduser/controller/UserController.java
@@ -2,8 +2,9 @@ package capston.capston_spring.controller;
 
 import capston.capston_spring.dto.*;
 import capston.capston_spring.entity.AppUser;
-import capston.capston_spring.service.PracticeSessionService;
+import capston.capston_spring.service.AccuracySessionService;
 import capston.capston_spring.service.UserService;
+import capston.capston_spring.service.VideoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -13,7 +14,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,7 +24,8 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final PracticeSessionService practiceSessionService;
+    private final AccuracySessionService accuracySessionService;
+    private final VideoService videoService;
 
     /**
      * 마이페이지 메인, my profile
@@ -100,8 +104,50 @@ public class UserController {
 
     /** my video
      * practie, challenge 비디오 확인 가능
-     * recording on/off 변경 가능
+     * edit 가능
      */
+    @GetMapping("/myVideo")
+    public Map<String, List<MyVideoResponse>> getMyVideoList(Authentication authentication){
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        AppUser user = this.userService.getUser(userDetails.getEmail());
+        Long userId = user.getId();
+
+        List<MyVideoResponse> practiceVideos = this.videoService.getPracticeVideos(userId);
+        List<MyVideoResponse> challengeVideos = this.videoService.getChallengeVideos(userId);
+
+        // PRACTICE와 CHALLENGE 구분하여 반환
+        Map<String, List<MyVideoResponse>> response = new HashMap<>();
+        response.put("practiceVideos", practiceVideos);
+        response.put("challengeVideos", challengeVideos);
+
+        return response;
+    }
+
+    @GetMapping("/myVideo/practice")
+    public List<MyVideoResponse> getMyVideoPracticeList(Authentication authentication){
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        AppUser user = this.userService.getUser(userDetails.getEmail());
+        Long userId = user.getId();
+
+        return this.videoService.getPracticeVideos(userId);
+    }
+
+    @GetMapping("/myVideo/challenge")
+    public List<MyVideoResponse> getMyVideoChallengeList(Authentication authentication){
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        AppUser user = this.userService.getUser(userDetails.getEmail());
+        Long userId = user.getId();
+
+        return this.videoService.getChallengeVideos(userId);
+    }
+
+    @PostMapping(value = "/myVideo/{videoId}/edit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadVideo(
+            @RequestParam("file") MultipartFile file,
+            @PathVariable("videoId") Long videoId  // 기존 영상의 ID를 받아옴
+    ){
+        return videoService.editVideo(videoId, file);
+    }
 
 
     /**
@@ -110,15 +156,15 @@ public class UserController {
      * 노래 이미지, 노래 제목, 가수, 점수(BEST, GOOD, BAD)
      */
     @GetMapping("/history")
-    public List<PracticeSessionRequest> getPracticeHistory(Authentication authentication) {
+    public List<AccuracySessionResponse> getPracticeHistory(Authentication authentication) {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         AppUser user = this.userService.getUser(userDetails.getEmail());
         Long userId = user.getId();
 
-        return this.practiceSessionService.getPracticeHistory(userId);
+        return this.accuracySessionService.getAccuracyHistory(userId);
     }
 
-    /** info App
+    /** info App  => 프론트에서 작성한 페이지 보여주기
      * Licentse페이지 이동
      * Help Center 페이지 이동
      */
