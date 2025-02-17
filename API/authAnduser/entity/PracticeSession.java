027154cce86e4a8294c864b308a1f4d68fdcf11b
kymEwhain <ymdwang200@ewhain.net>
2025-02-17 15:57:32 +0900

    entity 통일

--- API/authAnduser/entity/PracticeSession.java
+++ API/authAnduser/entity/PracticeSession.java
@@ -1,25 +1,36 @@
 package capston.capston_spring.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class PracticeSession {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private AppUser user;
 
     @ManyToOne
-    @JoinColumn(name = "song_id")
+    @JoinColumn(name = "song_id", nullable = false)
     private Song song;
+
+    @Column(nullable = false)   // 시작시간은 꼭 확인
+    private LocalDateTime startTime;
+
+    @Column
+    private LocalDateTime endTime;
 }
