a4ddd8e5365518c32b477643bcc0bf475fb5907b
kymEwhain <ymdwang200@ewhain.net>
2025-02-17 15:57:49 +0900

    entity 통일

--- API/authAnduser/entity/RecordedVideo.java
+++ API/authAnduser/entity/RecordedVideo.java
@@ -1,9 +1,7 @@
 package capston.capston_spring.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -11,17 +9,23 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class RecordedVideo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private AppUser user;   // 사용자 정보 (필수)
+    private AppUser user;
 
-    private String mode;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VideoMode mode;
 
     @ManyToOne
     @JoinColumn(name = "practice_session_id")
@@ -31,8 +35,13 @@ public class RecordedVideo {
     @JoinColumn(name = "challenge_session_id")
     private ChallengeSession challengeSession; // 챌린지 세션 (선택적 연결)
 
+    @Column(nullable = false)
     private String videoPath;         // 저장된 비디오 파일 경로
+
+    @Column(nullable = false)
     private LocalDateTime recordedAt; // 녹화 시간 -> 연결된 session의 시작 시간과 동일하게 맞춰야 할 듯 -> 혹은 없애는게 나을 수도?
+
+    @Column(nullable = false)
     private int duration;             // 영상 길이 (초 단위)
 
 }
