7911a421e3968acc2ed9f8ddeecafc8ac6e4f4b8
kymEwhain <ymdwang200@ewhain.net>
2025-03-17 18:48:29 +0900

    Delete API/songsAndVideos directory

--- API/songsAndVideos/dto/PlaybackRange.java
+++
@@ -1,9 +0,0 @@
-package com.example._4.dto;
-
-import lombok.Data;
-
-@Data
-public class PlaybackRange {
-    private int startTime;
-    private int endTime;
-}
--- API/songsAndVideos/dto/PlaybackSpeed.java
+++
@@ -1,8 +0,0 @@
-package com.example._4.dto;
-
-import lombok.Data;
-
-@Data
-public class PlaybackSpeed {
-    private double speed;
-}
