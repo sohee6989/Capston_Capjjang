51e666b83c6cfbc6c6ca275c5d709b861538e963
kymEwhain <ymdwang200@ewhain.net>
2025-03-17 18:48:20 +0900

    Delete API/authAnduser directory

--- API/authAnduser/repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package capston.capston_spring.repository;
-
-import capston.capston_spring.entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-}
