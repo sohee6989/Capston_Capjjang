226fcad354b5a5048c6f8ba4825424126dabf90d
kymEwhain <ymdwang200@ewhain.net>
2025-03-17 18:47:47 +0900

    Delete API/0221_final directory

--- API/0221_final/exception/VideoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package capston.capston_spring.exception;
-
-public class VideoNotFoundException extends RuntimeException {
-    public VideoNotFoundException(String message) {
-        super(message);
-    }
-}
