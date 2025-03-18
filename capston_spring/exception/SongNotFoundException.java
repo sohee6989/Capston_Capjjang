186bddf5f1dae6882769c15c9c60c353d7b0e916
dyoon17 <nkdy08@gmail.com>
2025-03-19 00:52:32 +0900

    Delete capston_spring directory

--- capston_spring/exception/SongNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package capston.capston_spring.exception;
-
-public class SongNotFoundException extends RuntimeException {
-    public SongNotFoundException(String message) {
-        super(message);
-    }
-}
