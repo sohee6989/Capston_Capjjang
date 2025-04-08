70324ec879bd1e34fc27b595c71a686d578ebe6a
Doyoon <nkdy08@gmail.com>
2025-04-08 12:51:20 +0900

    Create insert_songs_v2.sql
    cover_image_path: cover.jpg -> 곡이름_cover.jpg 형태로 변경
    challenge_start_time, challenge_end_time 필드 삭제(백엔드에서 하이라이트 구간 가져옴)

---
+++ API/mySQL/insert_songs_v2.sql
@@ -0,0 +1,89 @@
+INSERT INTO song (
+    title, artist, duration, genre,
+    full_start_time, full_end_time,
+    highlight_start_time, highlight_end_time,
+    audio_file_path, silhouette_video_path,
+    dance_guide_path, avatar_video_with_audio_path,
+    cover_image_path, created_by, bpm
+) VALUES (
+    'LoveDive', 'IVE', 178, 'K-Pop',
+    30, 90, 40, 70,
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/LoveDive/audio.mp3',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/LoveDive/LoveDive_silhouette.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/LoveDive/LoveDive_expert.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/LoveDive/avatar.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/LoveDive/LoveDive_cover.jpg',
+    'Sophie Lautenschlager', 112
+);
+
+INSERT INTO song (
+    title, artist, duration, genre,
+    full_start_time, full_end_time,
+    highlight_start_time, highlight_end_time,
+    audio_file_path, silhouette_video_path,
+    dance_guide_path, avatar_video_with_audio_path,
+    cover_image_path, created_by, bpm
+) VALUES (
+    'PinkVenom', 'BLACKPINK', 190, 'K-Pop',
+    20, 80, 30, 75,
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/PinkVenom/audio.mp3',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/PinkVenom/PinkVenom_silhouette.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/PinkVenom/PinkVenom_expert.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/PinkVenom/avatar.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/PinkVenom/PinkVenom_cover.jpg',
+    'Teddy Park', 90
+);
+
+INSERT INTO song (
+    title, artist, duration, genre,
+    full_start_time, full_end_time,
+    highlight_start_time, highlight_end_time,
+    audio_file_path, silhouette_video_path,
+    dance_guide_path, avatar_video_with_audio_path,
+    cover_image_path, created_by, bpm
+) VALUES (
+    'Butter', 'BTS', 164, 'K-Pop',
+    25, 85, 35, 65,
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Butter/audio.mp3',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Butter/Butter_silhouette.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Butter/Butter_expert.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Butter/avatar.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Butter/Butter_cover.jpg',
+    'Jenna Andrews', 110
+);
+
+INSERT INTO song (
+    title, artist, duration, genre,
+    full_start_time, full_end_time,
+    highlight_start_time, highlight_end_time,
+    audio_file_path, silhouette_video_path,
+    dance_guide_path, avatar_video_with_audio_path,
+    cover_image_path, created_by, bpm
+) VALUES (
+    'Ditto', 'NewJeans', 182, 'K-Pop',
+    15, 70, 25, 55,
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Ditto/audio.mp3',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Ditto/Ditto_silhouette.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Ditto/Ditto_expert.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Ditto/avatar.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Ditto/Ditto_cover.jpg',
+    '250', 130
+);
+
+INSERT INTO song (
+    title, artist, duration, genre,
+    full_start_time, full_end_time,
+    highlight_start_time, highlight_end_time,
+    audio_file_path, silhouette_video_path,
+    dance_guide_path, avatar_video_with_audio_path,
+    cover_image_path, created_by, bpm
+) VALUES (
+    'Antifragile', 'LE SSERAFIM', 200, 'K-Pop',
+    22, 82, 32, 72,
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Antifragile/audio.mp3',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Antifragile/Antifragile_silhouette.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Antifragile/Antifragile_expert.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Antifragile/avatar.mp4',
+    'https://danzle-s3-bucket.s3.ap-northeast-2.amazonaws.com/songs/Antifragile/Antifragile_cover.jpg',
+    'Score (13) & Megatone', 105
+);
